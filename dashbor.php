f86c74e633dc3761c97fc542889e831ce5dba22b
RPQ35 <alfajriropiq@gmail.com>
2024-11-21 10:04:59 +0800

    telat karna sakit kemaren

---
+++ dashbor.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="eng" id="dashborone">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=e, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+    <div class="content"></div>
+    <div class="dashbor">Dashbor</div>
+    <div class="masterB">Maste barang</div>
+    <div class="receive">Receiving</div>
+    <div class="isuu">Issuing</div>
+    </header>
+   <main>
+    <div class="top-dash">
+    <div class="ini">
+        <h3>username</h3>
+        <div class="img"></div>
+    </div>
+    </div>
+    <div class="data">
+        <input class="serc" type="search" placeholder="pencarian">
+        <table>
+            <tr>
+                <th>no</th>
+                <th>nama barang </th>
+                <th>pengadaan </th>
+                <th>stok</th>
+                <th>
+                    status
+                </th>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </table>
+    </div>
+    <div class="botton-dash">
+        <h3 class="kiri">copyright </h3>
+        <h3 class="kanan">versi kesekian</h3>
+    </div>
+   </main>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="eng"id=indexone>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+        <div id="laman-kiri">
+           <div class="isi-kiri">
+
+           </div>  
+        </div>
+        <div id="login">
+            <div class="konten-atas"></div>
+            <form action="dashbor.php">
+                <label for="username">Username:</label>
+                <input type="text" placeholder="admin1234">
+                <label for="password" >password</label>
+                <input type="password" name="pass" id="" placeholder="12345">
+                <button accesskey="submit" type="submit">submit</button>
+            </form>
+            <a href="">lupa passsword</a>
+        </div>
+    </main>
+</body>
+</html>
